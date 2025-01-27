302609c202b18f453f4c760990de8c9427c17d1b
IxxdbxxI <2193725898@qq.com>
2025-01-27 16:37:35 +0800

    "3"

--- local_configs/_base_/datasets/vspw_repeat2.py
+++ local_configs/_base_/datasets/vspw_repeat2.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'VSPWDataset2'
-data_root = 'data/vspw/VSPW_480p'
+data_root = '/kaggle/input/vspw480-all/VSPW_480p/VSPW_480p'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (480, 480)
