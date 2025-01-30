086e93cae15efc9eecda355ab3d356e4a7d5aab2
IxxdbxxI <2193725898@qq.com>
2025-01-30 11:31:35 +0800

    "test"

--- mmseg/apis/test_cluster.py
+++ mmseg/apis/test_cluster.py
@@ -40,7 +40,7 @@ def single_gpu_test(model,
                     data_loader,
                     show=False,
                     out_dir=None,
-                    efficient_test=False,lists=['23_XmN5TD3AjMY','50_9mZFBNGzmok','127_-hIVCYO4C90']):
+                    efficient_test=False,lists=['23_XmN5TD3AjMY','50_9mZFBNGzmok','127_-hIVCYO4C90','736_ML-JwZIxno0']):
     """Test with single GPU.
 
     Args:
