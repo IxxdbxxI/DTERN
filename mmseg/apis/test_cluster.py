faf002df566ecec448322c1a236e59916ae2af34
IxxdbxxI <2193725898@qq.com>
2025-01-30 12:02:10 +0800

    "code"

--- mmseg/apis/test_cluster.py
+++ mmseg/apis/test_cluster.py
@@ -112,7 +112,7 @@ def single_gpu_test(model,
 
                 assgined_result_i = target_assgined_result
                 assgined_result_maps = torch.sigmoid(assgined_result_i)
-                H_m = assgined_result_maps.view(b,num_clusters,o_h,o_w)
+                H_m = assgined_result_maps.view(b,num_clusters,106,60)
                 mean_scores = H_m.mean(dim=(2, 3), keepdim=True)  # [b,num_clusters,1,1] softmax是沿着空间维度的
                 
                 binary_masks = (H_m > mean_scores).float()  # 生成二值掩码 
